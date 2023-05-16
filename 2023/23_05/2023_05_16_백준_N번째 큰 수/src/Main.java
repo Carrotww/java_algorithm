8d42f64ccd761211d6ddf2ff44dc575da1d55025
Carrot_hyeong <rjgjfl90@naver.com>
2023-05-16 14:35:05 +0900

    백준 N번째 큰 수 자바

---
+++ 2023/23_05/2023_05_16_백준_N번째 큰 수/src/Main.java
@@ -0,0 +1,29 @@
+// https://www.acmicpc.net/problem/2075
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            pq.offer(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                pq.offer(Integer.parseInt(st.nextToken()));
+                pq.poll();
+            }
+        }
+
+        System.out.println(pq.poll());
+    }
+}
